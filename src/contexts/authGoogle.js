9ed363d1ecdabdc6dfb957700d84c0bd8d9e8bc8
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-06-04 22:57:20 -0300

    feat(auth): using error catch

--- src/contexts/authGoogle.js
+++ src/contexts/authGoogle.js
@@ -36,6 +36,7 @@ export const AuthGoogleProvider = ({ children }) => {
         const errorMessage = error.message;
         const email = error.email;
         const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(errorCode, errorMessage, email, credential);
       });
   }
 
